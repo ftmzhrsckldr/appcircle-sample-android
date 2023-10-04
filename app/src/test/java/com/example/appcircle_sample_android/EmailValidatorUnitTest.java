03bc637a6472dc701ce1ea55d45e6e6da09d2b75
ftmzhrsckldr <ftmzhrsckldr@protonmail.com>
2023-10-04 16:30:27 +0300

    Added email validator unit test

---
+++ app/src/test/java/com/example/appcircle_sample_android/EmailValidatorUnitTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+class EmailValidatorTest {
+  @Test
+  public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+    assertTrue(EmailValidator.isValidEmail("name@email.com"));
+  }
+
+  @Test
+  public void emailValidator_nonCorrectEmailSimple_ReturnsFalse() {
+    assertTrue(EmailValidator.isValidEmail("nameemailcom"));
+  }
+}
