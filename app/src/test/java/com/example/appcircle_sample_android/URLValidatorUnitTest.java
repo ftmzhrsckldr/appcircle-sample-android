a182b948a9105f3cbb144aaaee83e1adf29e20b6
ftmzhrsckldr <ftmzhrsckldr@protonmail.com>
2023-10-04 15:09:22 +0300

    Added logger

--- app/src/test/java/com/example/appcircle_sample_android/URLValidatorUnitTest.java
+++ app/src/test/java/com/example/appcircle_sample_android/URLValidatorUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+final Logger logger = LoggerFactory.getLogger(LoggingTest.class);
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
